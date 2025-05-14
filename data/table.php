1f965955d5d94652b9ab59370b2818125bf9c81e
niantique <nina.quetin@gmail.com>
2025-05-14 15:05:45 +0200

    page 1 form

---
+++ data/table.php
@@ -0,0 +1,34 @@
+<?php
+
+$Genders = ["female", "male", "other", "prefere not to say"];
+
+$users = [
+    ["id" => 1,
+    "lastname" => "Chloé",
+    "firstname" => "Allouche",
+    "email" => "chloe.allouche@gmail.com",
+    "address" => "1427 Sunset Blvd",
+    "city" => "Los Angeles",
+    "zipcode" => "90026",
+    "gender" => "female",
+],
+["id" => 2,
+    "lastname" => "Eve",
+    "firstname" => "Around",
+    "email" => "around@gmail.com",
+    "address" => "13 rd Eden",
+    "city" => "Los Angeles",
+    "zipcode" => "90026",
+    "gender" => "female",
+],
+["id" => 3,
+    "lastname" => "Angel",
+    "firstname" => "Pain",
+    "email" => "pain.angel@gmail.com",
+    "address" => "274 Sunrise Blvd",
+    "city" => "Los Angeles",
+    "zipcode" => "90026",
+    "gender" => "male",
+],
+]
+?>
